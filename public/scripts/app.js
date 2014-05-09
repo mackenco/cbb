569feb848bb86397b9dbaf713283577d7c20d72c
Colin MacKenzie <colinwmackenzie@gmail.com>
2014-05-09 15:10:37 -0400

    pause

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,29 @@
-var dataset = [
-                  [ 5,     20 ],
-                  [ 480,   90 ],
-                  [ 250,   50 ],
-                  [ 100,   33 ],
-                  [ 330,   95 ],
-                  [ 410,   12 ],
-                  [ 475,   44 ],
-                  [ 25,    67 ],
-                  [ 85,    21 ],
-                  [ 220,   88 ]
-              ];
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-var h = 100;
-var w = 500;
+var h = 250;
+var w = 600;
 var barPadding = 1;
+var padding = 30;
 
 var svg = d3.select("body")
-                    .append("svg")
-                    .attr("width", w)
-                    .attr("height", h);
+            .append("svg")
+            .attr({
+              height: h,
+              width: w
+            });
 
-svg.selectAll("circle")
-      .data(dataset)
-      .enter()
-      .append("circle")
-      .attr({
-        cx: function(d) { return d[0]; },
-        cy: function(d) { return d[1]; },
-        r: function(d) { return Math.sqrt(h - d[1]); }
-      });
-
-svg.selectAll("text")
-     .data(dataset)
-     .enter()
-     .append("text")
-     .text(function(d) { return d[0] + "," + d[1]; }) 
-    .attr({
-      x: function(d) { return d[0]; },
-      y: function(d) { return d[1]; },
-      "font-family": "sans-serif",
-      "font-size": "11px",
-      "fill": "red"
-     });
+svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr({
+    x: function(d, i) { return i * (w / dataset.length) ) },
+    y: function(d) { return h - (d * 4) },
+    height: function(d) { return d * 4 },
+    width: function(d) { return w / dataset.length - barPadding },
+    fill: function(d) { return "rgb(0"}
+   })
 
 
 // var h = 500;
